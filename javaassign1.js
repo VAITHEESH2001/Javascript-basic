b1eb57192bd4189ae3c0af72dbb6102490ba9ea3
Vaitheeshwaran S <155373392+VAITHEESH2001@users.noreply.github.com>
2024-04-05 16:51:56 +0530

    Add files via upload

---
+++ javaassign1.js
@@ -0,0 +1,13 @@
+var a = parseFloat(prompt("Enter the a value: "));
+var b = parseFloat(prompt("Enter the b value: "));
+
+var c = a + b;
+var d = a - b;
+var e = a * b;
+var f = a / b;
+
+console.log("Your Addition output is =", c);
+console.log("Your Subtraction output is =", d);
+console.log("Your multiplication output is =", e);
+console.log("Your Division output is =", f);
+
